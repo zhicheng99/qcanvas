f8c61c4df957629d1d96d1fb44014aee624f6533
123456 <lizhicheng99@163.com>
2020-09-30 00:09:10 +0800

    右键

--- flow/js/Qflow.js
+++ flow/js/Qflow.js
@@ -23,7 +23,7 @@ function Qflow(options){
 
 	this.draging = false;
 
-	this.contextMenuNode = null; //右键的对象
+	this.contextSettingNode = null; //设置菜单的对象
 	this.contextAimAttr = '';    //右键tab指向的属性
 
 
@@ -86,12 +86,19 @@ function Qflow(options){
 	this.initSettingIco();
 
 
+	//右键菜单
+	this.contextMenuLayer = this.qcanvas.qlayer.layer();
+	this.contextMenuLayer.setDisplay('none');
+	this.contextMenuNode = null; //右键菜单
+
+
+
+
 }
 Qflow.prototype.initSettingIco = function() {
 	var _this = this;
 	this.settingIco = this.qcanvas.qimg.img({
 		img:'./img/setting.png',
-		// img:'svg t="1601365617030" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3290" xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200"><defs><style type="text/css"></style></defs><path d="M512 959.8c-23.6 0-47.5-1.9-71.1-5.7-11-1.8-19.7-10.2-21.7-21.1-6.3-33.3-35.5-57.5-69.5-57.5-16 0-31.1 5.3-43.7 15.2-8.7 6.9-20.8 7.5-30.2 1.7-51.5-32-95.3-73.4-130.2-123.2-7-9.9-6.3-23.3 1.7-32.5 11.2-12.9 17.4-29.4 17.4-46.3 0-37.8-29.6-68.8-67.4-70.5-12.1-0.6-22.3-9.3-24.6-21.2-5.7-28.7-8.5-57.8-8.5-86.7 0-24.7 2.1-50 6.4-75.1 2.1-12.4 12.7-21.6 25.3-21.9 38.6-1 68.8-32.1 68.8-70.6 0-18.8-7.3-36.5-20.5-49.8-8.9-8.9-10.2-22.9-3.1-33.3 33.7-49.8 76.5-91.8 127.2-124.7 10.3-6.7 23.8-5.4 32.6 3.1 13.3 12.7 30.6 19.7 48.9 19.7 36.8 0 67.8-28.8 70.5-65.5 0.9-12.2 10.1-22.2 22.2-24.1 53.8-8.4 109.2-6.9 162.5 4.3 11.1 2.3 19.4 11.5 20.7 22.8 4.1 35.7 34.3 62.6 70.2 62.6 15.1 0 29.6-4.8 41.9-13.8 9.1-6.7 21.5-6.9 30.8-0.4 45.2 31.7 83.7 70.7 114.5 116.1 7.1 10.4 5.7 24.4-3.1 33.3-13.2 13.3-20.5 31-20.5 49.8 0 38.6 30.2 69.6 68.8 70.6 12.6 0.3 23.2 9.5 25.3 21.9 4.2 25.1 6.4 50.4 6.4 75.1 0 28.9-2.8 58.1-8.5 86.7-2.3 11.9-12.5 20.7-24.6 21.2-37.8 1.8-67.4 32.8-67.4 70.5 0 17 6.2 33.4 17.4 46.3 8 9.1 8.6 22.5 1.7 32.5-30.1 42.8-67.1 79.7-109.9 109.7-9.3 6.5-21.6 6.3-30.8-0.4-12.3-9.1-26.8-13.8-41.9-13.8-35.9 0-66.1 26.9-70.2 62.6-1.3 11.3-9.6 20.4-20.7 22.8-30.7 6.3-62 9.6-93.1 9.6z m-46.2-55.4c37.3 4.4 74.7 3.4 111.4-2.7C592 849.4 640 812 695.9 812c19.8 0 38.9 4.7 56.2 13.6 27.1-20.7 51.3-44.8 72.2-71.7-11.5-19.1-17.6-41-17.6-63.4 0-58.6 40.8-107.9 96.1-120.3 2.9-19.3 4.3-38.8 4.3-58.2 0-15.5-1-31.2-2.9-47-56.2-11.8-97.6-61-97.6-120.7 0-25.1 7.5-49.2 21.4-69.5-21.7-28.8-47.1-54.5-75.9-76.5-17.3 9-36.5 13.6-56.2 13.6-56 0-103.9-37.4-118.7-89.7-35.6-5.9-72-7-108-3.1-13.8 53.5-62.6 92.7-119.5 92.7-24.6 0-48.2-7.2-68.3-20.7-32.7 23.5-61.3 51.5-85.4 83.6 13.9 20.3 21.4 44.3 21.4 69.5 0 59.6-41.4 108.9-97.6 120.7-1.9 15.8-2.9 31.5-2.9 47 0 19.4 1.5 38.8 4.3 58.2 55.4 12.4 96.1 61.7 96.1 120.3 0 22.5-6.2 44.4-17.6 63.4 25.5 32.9 55.6 61.2 89.9 84.6 18.3-10.3 38.8-15.7 60.1-15.7 52.6 0.1 98.7 33.5 116.1 81.7z" fill="#333333" p-id="3291"></path><path d="M512 705.8c-106.9 0-193.8-87-193.8-193.8s87-193.8 193.8-193.8 193.8 87 193.8 193.8S618.9 705.8 512 705.8z m0-335c-77.8 0-141.2 63.3-141.2 141.2S434.2 653.1 512 653.1 653.2 589.8 653.2 512 589.8 370.8 512 370.8z" fill="#333333" p-id="3292"></path></svg>',
 		sStart:[0,0],
 		tStart:[0,0],
 		tWidth:10,
@@ -102,13 +109,13 @@ Qflow.prototype.initSettingIco = function() {
 			this.setDisplay('block');
 		},
 		mouseup:function(e,pos){ 
-			_this.contextMenuNode = this.contextMenuNode;
+			_this.contextSettingNode = this.contextSettingNode;
  
 
 			_this.qcanvas.raiseToTop(_this.contextSettingLayer);
-			// _this.contextMenuShow.call(_this.contextMenuNode,{x:this.sStart[0],y:this.sStart[1]});
-			// _this.contextMenuShow({x:this.sStart[0],y:this.sStart[1]});
-			_this.contextMenuShow(pos);
+			// _this.contextSettingShow.call(_this.contextSettingNode,{x:this.sStart[0],y:this.sStart[1]});
+			// _this.contextSettingShow({x:this.sStart[0],y:this.sStart[1]});
+			_this.contextSettingShow(pos);
 
 
 		}
@@ -121,7 +128,7 @@ Qflow.prototype.settingIcoShow = function(node) {
 	var y = start[1]+5; 
 
 
-	this.settingIco.contextMenuNode = node;
+	this.settingIco.contextSettingNode = node;
 	this.settingIco.setTStart([x,y]);
 	this.settingIco.setDisplay('block');
 	this.qcanvas.raiseToTop(this.settingIco);
@@ -143,11 +150,11 @@ Qflow.prototype.initContextCover = function() {
 			 opacity:0.1,
 			 drag:false, 
 			 mouseup:function(){
-			 	_this.contextMenuHide();
+			 	_this.contextSettingHide();
 			 }
 			}))
 };
-Qflow.prototype.contextMenuShow = function(pos) {
+Qflow.prototype.contextSettingShow = function(pos) {
  
  
 
@@ -164,7 +171,7 @@ Qflow.prototype.contextMenuShow = function(pos) {
 	this.initColorRect();
 
 	//删除按钮
-	this.initDelBtn();
+	// this.initDelBtn();
 
 	this.contextSettingLayer.setDisplay('block');
  
@@ -187,7 +194,7 @@ Qflow.prototype.initDelBtn = function() {
 				//删除操作
 				_this.delNode();
 
-				_this.contextMenuHide();
+				_this.contextSettingHide();
 			}
 		}),
 		this.qcanvas.qtext.text({
@@ -205,7 +212,7 @@ Qflow.prototype.modiTitle = function(v) {
 		this.modiTitleObj.setText(v);
 	}
 
-	var jsonObj = this.getJsonObj(this.contextMenuNode.id);
+	var jsonObj = this.getJsonObj(this.contextSettingNode.id);
 	jsonObj.text = v;
 
 };
@@ -233,9 +240,9 @@ Qflow.prototype.initModiTitleNode = function() {
 	d.style.height = h+'px';
 	d.style.display = 'block';
 
-	console.log(this.contextMenuNode);
+	console.log(this.contextSettingNode);
 
-	var rectJsonObj = this.getJsonObj(this.contextMenuNode.id);
+	var rectJsonObj = this.getJsonObj(this.contextSettingNode.id);
 	console.log(rectJsonObj);
 
 	if(rectJsonObj.attr && rectJsonObj.attr.titleId){
@@ -246,10 +253,10 @@ Qflow.prototype.initModiTitleNode = function() {
 
 
 };
-Qflow.prototype.contextMenuHide = function() {
+Qflow.prototype.contextSettingHide = function() {
 	var _this = this;
 
-	this.contextMenuNode = null;
+	this.contextSettingNode = null;
 	this.modiTitleObj = null;
 	this.contextSettingLayer.setDisplay('none'); 
 
@@ -315,7 +322,7 @@ Qflow.prototype.initColorRect = function() {
 
 				}else{
 
-					_this.contextMenuNode[_this.contextAimAttr] = this.fillColor;
+					_this.contextSettingNode[_this.contextAimAttr] = this.fillColor;
 					 
 				}
 
@@ -335,7 +342,7 @@ Qflow.prototype.updateNodeTitleColor = function(v) {
 	var getTitleNodeJSON = function(){ 
 
 		var tmp = _this.options.initData.node.filter(function(item){
-			return item.nodeId == _this.contextMenuNode.id;
+			return item.nodeId == _this.contextSettingNode.id;
 		})
 
 		if(tmp.length>0){
@@ -347,7 +354,7 @@ Qflow.prototype.updateNodeTitleColor = function(v) {
 			_this.options.initData.node.forEach(function(item){
 				if(item.nodeType == 'container'){
 					for (var i = 0; i < item.child.length; i++) {
-						if(item.child[i].nodeId == _this.contextMenuNode.id){
+						if(item.child[i].nodeId == _this.contextSettingNode.id){
 							tmp = item.child[i];
 							break;
 						}
@@ -378,7 +385,7 @@ Qflow.prototype.updateInitDataAttr = function(v) {
 	var getTitleNodeJSON = function(){ 
 
 		var tmp = _this.options.initData.node.filter(function(item){
-			return item.nodeId == _this.contextMenuNode.id;
+			return item.nodeId == _this.contextSettingNode.id;
 		})
 
 		if(tmp.length>0){
@@ -390,7 +397,7 @@ Qflow.prototype.updateInitDataAttr = function(v) {
 			_this.options.initData.node.forEach(function(item){
 				if(item.nodeType == 'container'){
 					for (var i = 0; i < item.child.length; i++) {
-						if(item.child[i].nodeId == _this.contextMenuNode.id){
+						if(item.child[i].nodeId == _this.contextSettingNode.id){
 							tmp = item.child[i];
 							break;
 						}
@@ -1125,12 +1132,12 @@ Qflow.prototype.createChildsOfContainer = function(parentNode,jsonObj,index) {
 				 	_this.draging = false;
 				 	//右击显示菜单
 				 	if(e.button == '2'){
-				 		_this.contextMenuNode = this;
+				 		_this.contextSettingNode = this;
 
 				 		//右键菜单层级放到最高
 				 		_this.qcanvas.raiseToTop(_this.contextSettingLayer);
 
-				 		_this.contextMenuShow.call(_this,pos);
+				 		_this.contextSettingShow.call(_this,pos);
 
 				 	}
 
@@ -1250,12 +1257,12 @@ Qflow.prototype.containerMouseUp = function(container,e,pos,jsonObj) {
  	//右击显示菜单
  	if(e.button == '2'){ 
 
- 		_this.contextMenuNode = container;
+ 		_this.contextSettingNode = container;
 
  		//右键菜单层级放到最高
  		_this.qcanvas.raiseToTop(_this.contextSettingLayer);
 
- 		_this.contextMenuShow(pos);
+ 		_this.contextSettingShow(pos);
 
  	}
 };
@@ -1385,6 +1392,12 @@ Qflow.prototype.addContainer = function(obj) {
 		 borderColor:'red', 
 		 fillColor:'',
 		 dashed:true, 
+		 getRangePoints:function(){ //返回rect边上的8个点的坐标 
+		 	return _this.createRangePoints(this.polyPoints());
+		 },
+		 mouseenter:function(){ 
+		 	_this.settingIcoShow(this);
+		 },
 		 mousedown:function(){
 
 		 	_this.containerMouseDown.call(_this,this,jsonObj); 
@@ -1395,10 +1408,14 @@ Qflow.prototype.addContainer = function(obj) {
 
 		 },
 		 mousemove:function(){
+		 	_this.settingIcoShow(this);
 
 		 	_this.containerMouseMove.call(_this,this,jsonObj); 
+		 },
+		 mouseout:function(){ 
+		 	_this.settingIcoHide(this);
+		 },
 
-		 }
 	})
 
 	this.qnodes.push(tmp);
@@ -1426,6 +1443,8 @@ Qflow.prototype.addContainer = function(obj) {
 		}
 	}
 	this.options.initData.node.push(jsonObj);
+
+	this.updateInitData(tmp,jsonObj);
 
 
 	//添加容器的标题
@@ -1623,6 +1642,7 @@ Qflow.prototype.addEle = function(obj) {
 		break;
 		case '1':
 			var tmp = this.inContainer(obj);//判断是否拖动到某了个容器里
+			console.log(tmp);
 			if(tmp.sign){
 				this.inSertToContainer(obj,tmp.aim);
 			}else{
